f14692552dbb8e8fe09937074e017001502350b2
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2020-02-06 15:00:52 -0500

    Implemented some components

--- tskfileviewer/src/TSKTreeItem.jsx
+++ tskfileviewer/src/TSKTreeItem.jsx
@@ -5,11 +5,39 @@ import TreeItem from "@material-ui/lab/TreeItem";
 import React, { Component } from "react";
 
 class TSKTreeItem extends Component {
+
+  constructor(props) {
+    super(props);
+    if(this.props.childrenCount == 0) {
+      this.state = {children : []};
+    } else {
+      this.state = {children : [{id: "x", name: "Loading..."}]}
+    }
+  }
+
+  handleOnClick = () => {
+    if(this.props.childrenCount == 0) return;
+
+    fetch("http://localhost:8080/v1/cases/test-datasources-endpoint_20200128_180031/files?parentID="+this.props.objectId)
+     .then((response) => {
+      return response.json();
+    }).then((treeItems) => {
+      console.log(treeItems);
+      this.setState({children : treeItems});
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <TreeItem
+        nodeId = {this.props.objectId}
+        label = {this.props.name}
+        onClick = {this.handleOnClick}
+      >
+        {this.state.children.map(response => 
+          <TSKTreeItem key = {response.id} objectId = {response.id.toString()} name = {response.name} childrenCount = {response.childrenCount} isFolder = {response.isDir}/>
+        )}
+      </TreeItem>
     );
   }
 }
--- tskfileviewer/src/TSKTreeView.jsx
+++ tskfileviewer/src/TSKTreeView.jsx
@@ -1,15 +1,34 @@
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import TreeView from "@material-ui/lab/TreeView";
+import TreeItem from "@material-ui/lab/TreeItem"
 
 import React, { Component } from "react";
+import TSKTreeItem from "./TSKTreeItem";
 
 class TSKTreeView extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {children : []};
+  }
+
+  handleNodeToggle = (event, nodes) => {
+    console.log("Tree view has already toggled the node");
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+        <TreeView
+          onNodeToggle = {this.handleNodeToggle}
+        >
+          <TSKTreeItem
+            key = "13580"
+            objectId = "13580"
+            name = "abc"
+          />
+        </TreeView>
     );
   }
 }
