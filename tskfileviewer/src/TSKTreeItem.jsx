7f17ce81bfe5742cdcb1b68a888d2ea2552dd8ad
rishwanth <rishwanth@basistech.com>
2020-02-06 17:08:40 +0000

    add required components

---
+++ tskfileviewer/src/TSKTreeItem.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import TreeItem from "@material-ui/lab/TreeItem";
+
+import React, { Component } from "react";
+
+class TSKTreeItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default TSKTreeItem;
---
+++ tskfileviewer/src/TSKTreeView.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import TreeView from "@material-ui/lab/TreeView";
+
+import React, { Component } from "react";
+
+class TSKTreeView extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default TSKTreeView;
